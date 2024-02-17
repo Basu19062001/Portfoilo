eb92d1f79a40cf9a2169c754cb344a687ddb1854
Basudev <samantabasu.2001@gmail.com>
2024-02-17 21:20:24 +0530

    First commit with Home or About page

---
+++ main.js
@@ -0,0 +1,30 @@
+/*==========================Show Menu=====================*/
+/*==========Menu Show===============*/
+/* Validate if constant exists*/
+
+/*=================Remove Menu Mobile=============== */
+/*=================Scroll Sections Active Link=============== */
+/*=================Change Backgraoud Header=============== */
+/*=================Show Roll Up=============== */
+/*=================About Tabs=============== */
+const tabs =  document.querySelectorAll('[data-target'),
+tabContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach((tab)=>{
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target);
+    
+   
+    tabContents.forEach((tabContent) => {
+        tabContent.classList.remove('tab__active');
+    });
+    
+    target.classList.add('tab__active');
+    
+    tabs.forEach((tab) => {
+        tab.classList.remove('tab__active');
+    });
+    tab.classList.add('tab__active');
+    });
+});
+/*=================Contact Form=============== */
