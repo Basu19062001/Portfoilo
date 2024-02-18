20aabc68654d226b4b84c79185c40fb86a690cdd
Basudev <samantabasu.2001@gmail.com>
2024-02-18 22:00:12 +0530

    Email authentication added through emailjs.com but unsuccessfully

--- main.js
+++ main.js
@@ -7,31 +7,74 @@
 /*=================Change Backgraoud Header=============== */
 /*=================Show Roll Up=============== */
 /*=================About Tabs=============== */
-const tabs =  document.querySelectorAll('[data-target'),
-tabContents = document.querySelectorAll('[data-content]');
+const tabs = document.querySelectorAll('[data-target'),
+    tabContents = document.querySelectorAll('[data-content]');
 
-tabs.forEach((tab)=>{
+tabs.forEach((tab) => {
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.target);
-    
-   
-    tabContents.forEach((tabContent) => {
-        tabContent.classList.remove('tab__active');
-    });
-    
-    target.classList.add('tab__active');
-    
-    tabs.forEach((tab) => {
-        tab.classList.remove('tab__active');
-    });
-    tab.classList.add('tab__active');
+
+
+        tabContents.forEach((tabContent) => {
+            tabContent.classList.remove('tab__active');
+        });
+
+        target.classList.add('tab__active');
+
+        tabs.forEach((tab) => {
+            tab.classList.remove('tab__active');
+        });
+        tab.classList.add('tab__active');
     });
 });
 /*=================Contact Form=============== */
 
 const contactForm = document.getElementById('contact-form'),
-contactName = document.getElementById('contact-name'),
-contactEmail = document.getElementById('contact-email'),
-contactSubject = document.getElementById('contact-subject'),
-contactMessage = document.getElementById('contact-message'),
-errorMessage = document.getElementById('error-messagee');
+    contactName = document.getElementById('contact-name'),
+    contactEmail = document.getElementById('contact-email'),
+    contactSubject = document.getElementById('contact-subject'),
+    contactMessage = document.getElementById('contact-message'),
+    errorMessage = document.getElementById('error-messagee');
+
+const sendEmail = (e) => {
+
+    e.preventDefault();
+    
+    //check if the field has a value 
+    if (contactName.value === '' ||
+        contactEmail.value === '' ||
+        contactSubject.value === '' ||
+        contactMessage.value === '') {
+        //Show message
+        errorMessage.textContent = 'Write all the input fields'
+    }
+    else {
+        // ServiceID, TempleID, #form, public key
+        emailjs.sendForm('service_8dd44bj',
+            'template_06usf0h',
+            'contact-form',
+            '497qMowTLPtw8FTDQ'
+        ).then(() => {
+            // Show message and add color, window + dot to open emoji
+            errorMessage.classList.add('color-first');
+            errorMessage.textContent = 'Message sent ✔️';
+
+            //After 5 second message will remove
+            setTimeout(() => {
+                errorMessage.textContent = '';
+
+            }, 5000);
+        }, (error) => {
+            alert('OOPS! SOMETING WENT WRONG...', error);
+        }
+        );
+
+        //Clear input fields
+        contactName.value = '';
+        contactEmail.value = '';
+        contactSubject.value = '';
+        contactMessage.value = '';
+
+    }
+};
+contactForm.addEventListener('submit', sendEmail);
