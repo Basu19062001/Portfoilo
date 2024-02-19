e34d42bbf50311f11e191095cf7a9922797dfe7d
Basudev <samantabasu.2001@gmail.com>
2024-02-19 23:56:30 +0530

    Scroll up added

--- main.js
+++ main.js
@@ -13,6 +13,15 @@ function scrollHeader(){
 }
 window.addEventListener('scroll',scrollHeader);
 /*=================Show Roll Up=============== */
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is greater than 350 viewport height, add the show-scroll class to scroll-top class
+    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll',scrollUp);
+
 /*=================About Tabs=============== */
 const tabs = document.querySelectorAll('[data-target'),
     tabContents = document.querySelectorAll('[data-content]');
